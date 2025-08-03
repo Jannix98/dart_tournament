3efa9e671e46d95a38b8bbe1a7293703b76c38aa
Jan-Niclas Feix <feix.1998@web.de>
2025-08-03 13:57:51 +0200

    temporary solution for the color theme

--- src/TestDemo/MainWindow.xaml.cs
+++ src/TestDemo/MainWindow.xaml.cs
@@ -1,7 +1,11 @@
 ﻿using DartTournament.WPF.Navigator;
 using DartTournament.WPF.Screens.StartScreen;
-using System.Windows;
+
 using DartTournament.WPF.ServiceManager;
+using MaterialDesignColors;
+using MaterialDesignColors.ColorManipulation;
+using MaterialDesignThemes.Wpf;
+using System.Windows;
 
 namespace TestDemo
 {
@@ -15,11 +19,34 @@ namespace TestDemo
         public MainWindow()
         {
             InitializeComponent();
+            App app = (App)Application.Current;
+            
             navigator = new ScreenNavigator();
             navigator.Initialize(MainFrame);
             navigator.NavigateTo(new StartScreenView(navigator));
+
+            SetPurpleThemeLight();
         }
 
-
+        /// <summary>
+        /// i didn't like the color of the purple theme, so I changed it.
+        /// this is a temporary solution, but it works.
+        /// </summary>
+        /// <param name="paletteHelper"></param>
+        /// <param name="theme"></param>
+        private static void SetPurpleThemeLight()
+        {
+            var swatches = new SwatchesProvider().Swatches;
+            var deepPurple = swatches.FirstOrDefault(x => x.Name == "deeppurple");
+            //Color color = Colors.Dee
+            PaletteHelper paletteHelper = new PaletteHelper();
+            Theme theme = paletteHelper.GetTheme();
+            var c700 = deepPurple.PrimaryHues.FirstOrDefault(x => x.Name == "Primary400");
+            theme.PrimaryLight = c700.Color.Lighten();
+            theme.PrimaryMid = c700.Color;
+            theme.PrimaryDark = c700.Color.Darken();
+            paletteHelper.SetTheme(theme);
+        }
     }
+
 }
