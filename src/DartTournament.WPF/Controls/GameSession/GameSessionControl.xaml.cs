af03dc899c263acb6c4d3c0d3e06379fef8ddb0a
Jan-Niclas Feix <feix.1998@web.de>
2025-08-16 15:07:01 +0200

    rearranged the tab control in the gamesession to lay the foundation to be replaced by a custom control later own

--- src/DartTournament.WPF/Controls/GameSession/GameSessionControl.xaml.cs
+++ src/DartTournament.WPF/Controls/GameSession/GameSessionControl.xaml.cs
@@ -6,6 +6,7 @@ using DartTournament.WPF.Utils.MatchHandler;
 using DartTournament.WPF.Utils.RoundCalculator;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -26,36 +27,28 @@ namespace DartTournament.WPF.Controls.GameSession
     /// </summary>
     public partial class GameSessionControl : UserControl
     {
-        private string _title;
-        private bool _showLooserRound;
-        List<DartPlayerUI> _players;
+        private readonly GameSessionVM _viewModel;
+        private readonly List<DartPlayerUI> _players;
 
         public GameSessionControl(string title, bool showLooserRound, List<DartPlayerUI> players)
         {
             InitializeComponent();
-            _title = title;
-            _showLooserRound = showLooserRound;
-            TitleTextBlock.Text = _title;
             
-            
-            _title = title;
+            _viewModel = new GameSessionVM(title, showLooserRound);
             _players = players;
+            
+            DataContext = _viewModel;
             Loaded += GameSessionControl_Loaded;
         }
 
         private void GameSessionControl_Loaded(object sender, RoutedEventArgs e)
         {
-            
-            if(_showLooserRound)
+            if(_viewModel.ShowLooserRound)
             {
-                // TODO: move those tabs into a separate control
-                // add loser control
-                
+                // Create looser game content
                 GameDataCreator looserGameCreator = new GameDataCreator(new LooserRoundCalculator());
                 LooserMatchCreator looserMatchCreator = new LooserMatchCreator(looserGameCreator);
                 var looserGameData = looserMatchCreator.Create(_players.Count, _players);
-
-                this.TabControl.Visibility = Visibility.Visible;
 
                 var matches = MatchGenerator.FromRounds(looserGameData.GameRounds);
                 var looserMatches = MatchGenerator.FromRounds(looserGameData.LooserRounds);
@@ -64,25 +57,24 @@ namespace DartTournament.WPF.Controls.GameSession
                 var gameControlMatchHandler = new GameMatchHandler(matches, looserMatchHandler);
                 var looserGameControlMatchHandler = new GameMatchHandler(looserMatches, null);
 
-                var control = GameTreeControl.GameTreeControl.CreateGame(gameControlMatchHandler);
+                var gameControl = GameTreeControl.GameTreeControl.CreateGame(gameControlMatchHandler);
                 var looserControl = GameTreeControl.GameTreeControl.CreateGame(looserGameControlMatchHandler);
-                GameTab.Content = control;
-                LooserRoundTab.Content = looserControl;
+                
+                _viewModel.SetGameContent(gameControl);
+                _viewModel.SetLooserRoundContent(looserControl);
             }
             else
             {
+                // Create normal game content
                 GameDataCreator normalGameCreator = new GameDataCreator(new NormalRoundCalculator());
                 NormalMatchCreator normalMatchCreator = new NormalMatchCreator(normalGameCreator);
                 var normalGameData = normalMatchCreator.Create(_players.Count, _players);
 
                 var matches = MatchGenerator.FromRounds(normalGameData.GameRounds);
-
                 var gameControlMatchHandler = new GameMatchHandler(matches, null);
-
                 var control = GameTreeControl.GameTreeControl.CreateGame(gameControlMatchHandler);
 
-                this.TabControl.Visibility = Visibility.Collapsed;
-                GameContent.Content = control;
+                _viewModel.SetGameContent(control);
             }
         }
     }
---
+++ src/DartTournament.WPF/Controls/GameSession/GameSessionVM.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace DartTournament.WPF.Controls.GameSession
+{
+    public class GameSessionVM : INotifyPropertyChanged
+    {
+        private string _title;
+        private bool _showLooserRound;
+        private int _selectedTabIndex;
+        private UserControl _gameContent;
+        private UserControl _looserRoundContent;
+
+        public string Title
+        {
+            get => _title;
+            set => SetProperty(ref _title, value);
+        }
+
+        public bool ShowLooserRound
+        {
+            get => _showLooserRound;
+            set => SetProperty(ref _showLooserRound, value);
+        }
+
+        public int SelectedTabIndex
+        {
+            get => _selectedTabIndex;
+            set
+            {
+                if (SetProperty(ref _selectedTabIndex, value))
+                {
+                    UpdateCurrentContent();
+                }
+            }
+        }
+
+        public UserControl CurrentContent
+        {
+            get
+            {
+                return SelectedTabIndex switch
+                {
+                    0 => _gameContent,
+                    1 => _looserRoundContent,
+                    _ => _gameContent
+                };
+            }
+        }
+
+        public GameSessionVM(string title, bool showLooserRound)
+        {
+            _title = title;
+            _showLooserRound = showLooserRound;
+            _selectedTabIndex = 0;
+        }
+
+        public void SetGameContent(UserControl content)
+        {
+            _gameContent = content;
+            UpdateCurrentContent();
+        }
+
+        public void SetLooserRoundContent(UserControl content)
+        {
+            _looserRoundContent = content;
+            UpdateCurrentContent();
+        }
+
+        private void UpdateCurrentContent()
+        {
+            OnPropertyChanged(nameof(CurrentContent));
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected bool SetProperty<T>(ref T field, T value, [CallerMemberName] string propertyName = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(field, value))
+                return false;
+
+            field = value;
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+    }
+}
